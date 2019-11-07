1075f22cb6a67bba32ce6e981a8efb0b59ff7596
Elyas Benyoub <ebenyoub@me.com>
2019-11-07 19:24:22 +0100

    libft terminee

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_memcmp.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: ebenyoub <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/14 15:24:46 by ebenyoub     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/14 17:11:05 by ebenyoub    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char *p1;
+	const unsigned char *p2;
+
+	p1 = s1;
+	p2 = s2;
+	while (n--)
+	{
+		if (*p1 != *p2)
+			return (*p1 - *p2);
+		else
+		{
+			p1++;
+			p2++;
+		}
+	}
+	return (0);
+}
---
+++ ft_split.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_split.c                                       .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: ebenyoub <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/16 16:10:18 by ebenyoub     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/17 17:28:33 by ebenyoub    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		count_wd(char *str, char c)
+{
+	int		i;
+	int		nb;
+
+	i = 0;
+	nb = 0;
+	while (str[i])
+	{
+		if (str[i] == c || str[i] == '\0')
+		{
+			while (str[i] && str[i] == c)
+				i++;
+			nb++;
+		}
+		else
+			i++;
+	}
+	return (nb);
+}
+
+static int		*word_mark(char *str, char c)
+{
+	int		i;
+	int		a;
+	int		nb;
+	int		*tab;
+
+	i = 0;
+	a = 1;
+	nb = count_wd(str, c);
+	tab = malloc(sizeof(int) * (nb + 1));
+	tab[0] = 0;
+	while (str[i] && (a <= nb))
+	{
+		if (str[i] == c)
+		{
+			while (str[i] == c)
+				i++;
+			tab[a] = i;
+			a++;
+		}
+		i++;
+	}
+	return (tab);
+}
+
+static char		*malloc_word(char *str, char c)
+{
+	int		i;
+	int		a;
+	char	*word;
+
+	i = 0;
+	a = 0;
+	if (!str[0])
+		return (NULL);
+	while (str[i] && str[i] != c)
+		i++;
+	word = malloc(sizeof(char) * i);
+	if (word == NULL)
+		return (NULL);
+	while (a < i)
+	{
+		word[a] = str[a];
+		a++;
+	}
+	word[a] = '\0';
+	return (word);
+}
+
+static char		**make_tab(char *str, char c)
+{
+	int		i;
+	int		*mark;
+	char	**tab;
+
+	i = 0;
+	tab = malloc(sizeof(char *) * (count_wd(str, c) + 1));
+	if (tab == NULL)
+		return (NULL);
+	mark = malloc(sizeof(int) * count_wd(str, c));
+	if (mark == NULL)
+		return (NULL);
+	mark = word_mark(str, c);
+	while (i <= count_wd(str, c))
+	{
+		tab[i] = malloc_word(&str[mark[i]], c);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	int		i;
+	char	*set;
+	char	*str;
+	char	**tab;
+
+	if (s)
+	{
+		i = 0;
+		if (!(set = malloc(sizeof(char))))
+			return (NULL);
+		set[0] = c;
+		str = ft_strtrim(s, set);
+		tab = make_tab(str, c);
+		return (tab);
+	}
+	return (NULL);
+}
